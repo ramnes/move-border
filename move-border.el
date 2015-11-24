e95211dc25c2490ebf237ce2a77892b0d28c2812
Albin Ludvig Otterhäll <AlbinLOtterhaell@gmail.com>
2015-11-24 11:27:34 +0100

    Add header

--- move-border.el
+++ move-border.el
@@ -1,3 +1,23 @@
+;;; move-border.el --- Move border to resize windows
+
+;; Copyright (C) Unknown
+
+;; Author: Unknown
+;; Maintainer: Guillaume Gelin <contact@ramnes.eu>
+;; Created 5 sep 2008
+;; Keywords: window border
+;; Homepage: https://github.com/ramnes/move-border
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Add commands to move border
+;; instead of using the built-in
+;; window resizing functions
+
+;;; Code:
+
 (defun xor (b1 b2)
   (or (and b1 b2)
       (and (not b1) (not b2))))
@@ -39,3 +59,6 @@
   (move-border-up-or-down arg nil))
 
 (provide 'move-border)
+
+;;; move-border.el ends here
+
